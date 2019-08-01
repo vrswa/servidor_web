e95fd55ef7555a7ff3aa6216f408e0b63f352463
daniel.mamani@vreadynow.com <daniel.mamani@vreadynow.com>
2019-08-01 16:50:41 -0300

    importado Message

--- src/ui/0prelude.js
+++ src/ui/0prelude.js
@@ -1,7 +1,7 @@
 //INFO: vars globales, inicializa, etc.
 
 var { Component, h, render } = window.preact;
-var { Header, Icon, Image, Menu, Segment, Sidebar, Button, Input, List, Item, Container, Form }= window.semanticUIReact;
+var { Header, Icon, Image, Menu, Segment, Sidebar, Button, Input, List, Item, Container, Form, Message }= window.semanticUIReact;
 
 render_str= preactRenderToString;
 
